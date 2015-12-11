98d4401aa132b393d0a1fefc658f3381fa644a11
Ricky Donato <rickj07@gmail.com>
2015-12-11 22:50:19 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,4 +3,8 @@ from setuptools import setup
 setup(
     name="threader",
     version="1.0",
+    author = "Ricky Donato",
+    author_email = "123@gmail.com",
+    description = ("Multi-threading module, document, and publish "
+                                   "to the cheese shop a5 pypi.org.")
 )
